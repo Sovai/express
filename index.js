d42f6f3a83d5181ad65533da3c587c1335844676
Sovai <kean.sovai@ababnk.com>
2022-01-14 17:57:02 +0700

    update origin domain

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "https://http-only-front-end.vercel.app",
+    origin: "https://modest-hypatia-ed4230.netlify.app",
     credentials: true,
     methods: ["GET", "PUT", "POST"],
   })
