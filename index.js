c7653e04bb1d1c5a26386945753960ab24d423a3
Sovai <kean.sovai@ababnk.com>
2022-01-15 10:57:48 +0700

    revert

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var date = new Date();
 // add a day
 date.setDate(date.getDate() + 1);
 app.get("/setcookie", (req, res) => {
-  res.cookie(`access_token`, `encrypted cookie string Value`, {
+  res.cookie(`access-token`, `encrypted cookie string Value`, {
     maxAge: 50000,
     // expires works the same as the maxAge
     expires: date,
